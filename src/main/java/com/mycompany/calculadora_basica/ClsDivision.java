d56cd99efeea0bcf24ece23eb31c20911bac0a89
LeynerPalma <108591091+LeynerPalma@users.noreply.github.com>
2022-07-03 01:42:11 -0500

    Update ClsDivision.java

--- src/main/java/com/mycompany/calculadora_basica/ClsDivision.java
+++ src/main/java/com/mycompany/calculadora_basica/ClsDivision.java
@@ -12,26 +12,36 @@ public class ClsDivision {
     }
     
     public void division (){
-        DecimalFormat dc = new DecimalFormat("#.##"); //esto solo me deja 2 decimales 
+        DecimalFormat dc = new DecimalFormat("#.##");//esto solo me deja 2 decimales 
         JOptionPane.showMessageDialog(null, "Recuerda que no puedes dividir entre (0)");//mensaje de notificación
         String division1 = JOptionPane.showInputDialog("Ingresa el primer numero");
         String division2 = JOptionPane.showInputDialog("Ingresa el segundo numero");
-        Double d1 = Double.parseDouble(division1);
-        Double d2 = Double.parseDouble(division2);
-        //valido que el segundo numero no sea un cero        
-        if (d2 == 0){
-            JOptionPane.showMessageDialog(null, "Lo lamento, pero no es posible dividir entre 0");
-           //me tira un sms y me devuelve al menu de opciones si se ingresa un 0
-            ClsInicio inicio = new ClsInicio();
-            inicio.inicio();
-        }
         
-        else{
-            Double totaldivision = d1/d2;
-            JOptionPane.showMessageDialog(null, "El resultado de la operación es: " + dc.format(totaldivision));
-             //esto hace la operación de la división y luego de mostrar el resultado me devuelve al menu de opciones
+        
+        try{
+        
+            Double d1 = Double.parseDouble(division1);//paso los string a double
+            Double d2 = Double.parseDouble(division2);
+            //valido que el segundo numero no sea un cero      
+            if (d2 == 0 || d2 == 0.0){
+                JOptionPane.showMessageDialog(null, "Lo lamento, pero no es posible dividir entre 0");
+               //me tira un sms y me devuelve al menu de opciones
                 ClsInicio inicio = new ClsInicio();
                 inicio.inicio();
+            }
+            //me hace la operación de la división
+            else{
+                Double totaldivision = d1/d2;
+                JOptionPane.showMessageDialog(null, "El resultado de la operación es: " + dc.format(totaldivision));
+                //me regresa al menú principal
+                    ClsInicio inicio = new ClsInicio();
+                    inicio.inicio();
+            }
+        }
+        catch(NumberFormatException ex){//si se genera la exepcion me tira sms y me tira al menu de opciones
+            JOptionPane.showMessageDialog(null,"Debes de ingresar un valor numerico");
+            ClsInicio inicio = new ClsInicio();
+            inicio.inicio();
         }
     
     
