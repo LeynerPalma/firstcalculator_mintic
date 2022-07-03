faf32daed96827f993f77ca98f17881c40af2651
LeynerPalma <108591091+LeynerPalma@users.noreply.github.com>
2022-07-03 01:39:49 -0500

    Update ClsPontencia.java

--- src/main/java/com/mycompany/calculadora_basica/ClsPontencia.java
+++ src/main/java/com/mycompany/calculadora_basica/ClsPontencia.java
@@ -1,8 +1,6 @@
 package com.mycompany.calculadora_basica;
 import javax.swing.JOptionPane;
 import java.text.DecimalFormat;
-//importo las funciones a utilizar 
-
 /**
  *
  * @author leiner
@@ -11,14 +9,24 @@ public class ClsPontencia {
     public ClsPontencia() {
     }
     public void potencia (){
-        DecimalFormat dc = new DecimalFormat("#.##");//me reduce el numero de decimales a solo dos 
-        String pote1 = JOptionPane.showInputDialog("Ingrese el primer numero");
+        DecimalFormat dc = new DecimalFormat("#.##");//me deja solo 2 decimales 
+        String pote1 = JOptionPane.showInputDialog("Ingrese el primer numero");//solicito los numero a trabajar 
         String pote2 = JOptionPane.showInputDialog("Ingresa el segundo numero");
-        Double p1 = Double.parseDouble(pote1);// paso los string a souble 
-        Double p2 = Double.parseDouble(pote2);
-        Double totalpotencia = Math.pow(p1, p2);//utilizo el Math que ya esta integrado en java y la funcion pow para calcular la potencia entre 1 numero multiplicado por la contidad de veces del otro 
-        JOptionPane.showMessageDialog(null, "El resultado de la operación es: " + dc.format(totalpotencia)); //me muestra el resultado y me muestra el menu principa 
-                ClsInicio inicio = new ClsInicio();
-                inicio.inicio();
+        
+        try{//si no se genera la exepcion me ejecuta el codigo
+        
+            Double p1 = Double.parseDouble(pote1);//paso de string a double
+            Double p2 = Double.parseDouble(pote2);
+            Double totalpotencia = Math.pow(p1, p2);//con este metodo saco la potencia del primer numero
+            JOptionPane.showMessageDialog(null, "El resultado de la operación es: " + dc.format(totalpotencia));
+                    ClsInicio inicio = new ClsInicio();
+                    inicio.inicio();//luego del mensaje muestro el menu de inicio 
+        }
+        catch(NumberFormatException ex){//si se ejecuta la exepcion me muestra el sms y me tira al menu principal
+            JOptionPane.showMessageDialog(null,"Debes de ingresar un valor numerico");
+            ClsInicio inicio = new ClsInicio();
+            inicio.inicio();
+
+        }
     }
 }
