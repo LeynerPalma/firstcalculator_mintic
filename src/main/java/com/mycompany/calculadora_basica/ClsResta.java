c4013ecc9c65b205ee7099fbab72127b37f859ea
LeynerPalma <108591091+LeynerPalma@users.noreply.github.com>
2022-07-03 01:38:37 -0500

    Update ClsResta.java

--- src/main/java/com/mycompany/calculadora_basica/ClsResta.java
+++ src/main/java/com/mycompany/calculadora_basica/ClsResta.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 
 /**
  *
- * @author leine
+ * @author leiner
  */
 public class ClsResta {
 
@@ -12,16 +12,28 @@ public class ClsResta {
     } // hay que generar las exepciones
    
     public void resta(){
-        DecimalFormat dc = new DecimalFormat("#.##");
+        DecimalFormat dc = new DecimalFormat("#.##");//me permite solo 2 decimales
         String resta1 = JOptionPane.showInputDialog("Ingrese el primer numero");
         String resta2 = JOptionPane.showInputDialog("Ingrese el segundo numero");
-        Double r1 = Double.parseDouble(resta1);
-        Double r2 = Double.parseDouble(resta2);
-        Double totalresta = r1-r2;
         
-        JOptionPane.showMessageDialog(null, "El valor de la resta es: " + dc.format(totalresta));
+        try{ //si no se genera la exepcion me ejecuta este codigo
+        
+        
+            Double r1 = Double.parseDouble(resta1);
+            Double r2 = Double.parseDouble(resta2);//paso de string a double
+            Double totalresta = r1-r2;
+
+            JOptionPane.showMessageDialog(null, "El valor de la resta es: " + dc.format(totalresta));
+                ClsInicio inicio = new ClsInicio();
+                inicio.inicio();
+
+        }
+        catch(NumberFormatException ex){//si se genera la exepción me muestra el mensaje y me tira al menu principal
+            JOptionPane.showMessageDialog(null,"Debes de ingresar un valor numerico");
             ClsInicio inicio = new ClsInicio();
             inicio.inicio();
+        }
+        
 
     
     }
