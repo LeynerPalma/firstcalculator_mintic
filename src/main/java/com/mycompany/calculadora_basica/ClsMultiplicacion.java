1526903ea16c044c77b72cacfc1f0c4e2d2c64b3
LeynerPalma <108591091+LeynerPalma@users.noreply.github.com>
2022-07-03 00:14:16 -0500

    Update ClsMultiplicacion.java

--- src/main/java/com/mycompany/calculadora_basica/ClsMultiplicacion.java
+++ src/main/java/com/mycompany/calculadora_basica/ClsMultiplicacion.java
@@ -1,23 +1,23 @@
 package com.mycompany.calculadora_basica;
 import javax.swing.JOptionPane;
 import java.text.DecimalFormat;
-
+//importo la fuciones a utilizar 
 /**
  *
- * @author leine
+ * @author leiner
  */
 public class ClsMultiplicacion {
     public ClsMultiplicacion() {
     }//hay que generar las exepciones
     public void multiplicacion(){
-        DecimalFormat dc = new DecimalFormat("#.##");
+        DecimalFormat dc = new DecimalFormat("#.##");//me reduce el nuemero de decimales a solo 2
         String multi1 = JOptionPane.showInputDialog("Ingrese el primer número");
         String multi2 = JOptionPane.showInputDialog("Ingrese el segundo número");
         Double m1 = Double.parseDouble(multi1);
         Double m2 = Double.parseDouble(multi2);
-        Double totalmulti = m1*m2;
+        Double totalmulti = m1*m2;//realiza la operación 
         
-        
+        //muestra el resultado y me regresa al menu de opciones 
         JOptionPane.showMessageDialog(null, "El resultado de la operación es: " + dc.format(totalmulti));
             ClsInicio inicio = new ClsInicio();
             inicio.inicio();
