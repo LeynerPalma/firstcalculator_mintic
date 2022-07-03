c3f96cdad7338d2a3356d7956ef40353dc33da83
LeynerPalma <108591091+LeynerPalma@users.noreply.github.com>
2022-07-03 00:17:33 -0500

    Update ClsPontencia.java

--- src/main/java/com/mycompany/calculadora_basica/ClsPontencia.java
+++ src/main/java/com/mycompany/calculadora_basica/ClsPontencia.java
@@ -1,21 +1,23 @@
 package com.mycompany.calculadora_basica;
 import javax.swing.JOptionPane;
 import java.text.DecimalFormat;
+//importo las funciones a utilizar 
+
 /**
  *
- * @author leine
+ * @author leiner
  */
 public class ClsPontencia {
     public ClsPontencia() {
     }
     public void potencia (){
-        DecimalFormat dc = new DecimalFormat("#.##");
+        DecimalFormat dc = new DecimalFormat("#.##");//me reduce el numero de decimales a solo dos 
         String pote1 = JOptionPane.showInputDialog("Ingrese el primer numero");
         String pote2 = JOptionPane.showInputDialog("Ingresa el segundo numero");
-        Double p1 = Double.parseDouble(pote1);
+        Double p1 = Double.parseDouble(pote1);// paso los string a souble 
         Double p2 = Double.parseDouble(pote2);
-        Double totalpotencia = Math.pow(p1, p2);
-        JOptionPane.showMessageDialog(null, "El resultado de la operación es: " + dc.format(totalpotencia));
+        Double totalpotencia = Math.pow(p1, p2);//utilizo el Math que ya esta integrado en java y la funcion pow para calcular la potencia entre 1 numero multiplicado por la contidad de veces del otro 
+        JOptionPane.showMessageDialog(null, "El resultado de la operación es: " + dc.format(totalpotencia)); //me muestra el resultado y me muestra el menu principa 
                 ClsInicio inicio = new ClsInicio();
                 inicio.inicio();
     }
